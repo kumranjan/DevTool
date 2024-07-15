6e5f26fc7c4f2517458fd37da65bc6ff2d708b85
Dell <Dell@DESKTOP-E0TU9MH>
2024-07-16 00:46:32 +0530

    By Dev--> Unit test byPositiveNumber

--- GITProject01/src/main/java/com/nt/test/Arithmetic.java
+++ GITProject01/src/main/java/com/nt/test/Arithmetic.java
@@ -2,4 +2,8 @@ package com.nt.test;
 
 public class Arithmetic {
 
+	public int Sum(int x,int y)
+	{
+		return x+y;
+	}
 }
--- GITProject01/src/test/java/com/nt/test/AppTest.java
+++ GITProject01/src/test/java/com/nt/test/AppTest.java
@@ -1,7 +1,19 @@
 package com.nt.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class AppTest 
 {
+	@Test
+	public void testWithPositive()
+	{
+		Arithmetic ar=new Arithmetic();
+		int actual=ar.Sum(10,20);
+		int expected=30;
+		assertEquals(expected,actual);
+		
+	}
     
 }
