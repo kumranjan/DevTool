0e6100f978701ed535c2cda7acc9c8eaf74ecaaa
Dell <Dell@DESKTOP-E0TU9MH>
2024-07-16 01:37:25 +0530

    delete testWithMixedNUmber() by TL

--- GITProject01/src/test/java/com/nt/test/AppTest.java
+++ GITProject01/src/test/java/com/nt/test/AppTest.java
@@ -26,16 +26,8 @@ public class AppTest
 		assertEquals(expected,actual);
 		
 	}
-	@Test
-	public void testWithMixedNUmber()
-	{
-		System.out.println("AppTest.testWithMixedNUmber()");
-		Arithmetic ar=new Arithmetic();
-
-		int actual=ar.Sum(-100,120);
-		int expected=20;
-		assertEquals(expected,actual);
+	
 		
 	}
     
-}
+
