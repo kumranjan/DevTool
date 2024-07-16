af174bc7972bc082d2ed598d5dbae0f8a9e5c386
Dell <Dell@DESKTOP-E0TU9MH>
2024-07-16 17:09:46 +0530

    change line 12

--- GITProject01/src/test/java/com/nt/test/AppTest.java
+++ GITProject01/src/test/java/com/nt/test/AppTest.java
@@ -9,7 +9,7 @@ public class AppTest
 	@Test
 	public void testWithPositive()
 	{
-		System.out.println("AppTest.testWithPositive()");
+		System.out.println("AppTest.testWithPositive() begin()");
 		Arithmetic ar=new Arithmetic();
 
 		int actual=ar.Sum(20,30);
